65d2944ea21e36726447d6882409dd14ba2b212e
BartekZakrzewski <bartek.zakrzewski3012@gmail.com>
2026-03-10 14:37:15 +0000

    Moved primlib to rotating_polygon dir

--- rotating_polygon/rotating_polygon.c
+++ rotating_polygon/rotating_polygon.c
@@ -1,4 +1,4 @@
-#include "../primlib/primlib.h"
+#include "primlib.h"
 #include <stdlib.h>
 #include <unistd.h>
 #include <math.h>
@@ -10,59 +10,59 @@
 #define ROTATION 0.02
 
 typedef struct {
-  int x;
-  int y;
+    int x;
+    int y;
 } Point;
 
 const double angle_between_points = (2.0 * M_PI) / N_VERTICES;
 
-void calculate_point_rotation(Point *point, double *rotation_offset, Point *center, int *radius);
+void calculate_points_position(Point *point, double *rotation_offset, Point *center, int *radius);
 void draw_polygon(Point *point);
 
 int main(int argc, char* argv[]) {
-  if (gfx_init()) {
-    exit(3);
-  }
-
-  Point points[N_VERTICES];
-  double rotation_offset = 0.0;
-  int radius = MIN_RADIUS + RADIUS_STEP;
-  int grow_direction = 1;
-
-  Point center; center.x = gfx_screenWidth() / 2; center.y = gfx_screenHeight() / 2;
-
-  while (1) {
-    gfx_filledRect(0, 0, gfx_screenWidth() - 1, gfx_screenHeight() - 1, BLACK);
-
-    calculate_point_rotation(points, &rotation_offset, &center, &radius);
-    rotation_offset = fmod(rotation_offset + ROTATION, 2.0 * M_PI);
-    grow_direction = radius >= MAX_RADIUS || radius <= MIN_RADIUS ? -1 * grow_direction : grow_direction;
-    radius += grow_direction * RADIUS_STEP;
-
-    draw_polygon(points);
-
-    if (gfx_pollkey() == 3) {
-      break;
+    if (gfx_init()) {
+        exit(3);
     }
 
-    gfx_updateScreen();
-    SDL_Delay(10);
-  }
+    Point points[N_VERTICES];
+    double rotation_offset = 0.0;
+    int radius = MIN_RADIUS + RADIUS_STEP;
+    int grow_direction = 1;
 
-  return 0;
+    Point center; center.x = gfx_screenWidth() / 2; center.y = gfx_screenHeight() / 2;
+
+    while (1) {
+        gfx_filledRect(0, 0, gfx_screenWidth() - 1, gfx_screenHeight() - 1, BLACK);
+
+        calculate_points_position(points, &rotation_offset, &center, &radius);
+        rotation_offset = fmod(rotation_offset + ROTATION, 2.0 * M_PI);
+        grow_direction = radius >= MAX_RADIUS || radius <= MIN_RADIUS ? -1 * grow_direction : grow_direction;
+        radius += grow_direction * RADIUS_STEP;
+
+        draw_polygon(points);
+
+        if (gfx_pollkey() == 3) {
+            break;
+        }
+
+        gfx_updateScreen();
+        SDL_Delay(10);
+    }
+
+    return 0;
 }
 
-void calculate_point_rotation(Point *point, double *rotation_offset, Point *center, int *radius) {
-  for (int vertex = 0; vertex < N_VERTICES; vertex++) {
-    double angle = vertex * angle_between_points + *rotation_offset;
-    (point + vertex)->x = center->x + *radius * cos(angle);
-    (point + vertex)->y = center->y + *radius * sin(angle);
-  }
+void calculate_points_position(Point *point, double *rotation_offset, Point *center, int *radius) {
+    for (int vertex = 0; vertex < N_VERTICES; vertex++) {
+        double angle = vertex * angle_between_points + *rotation_offset;
+        (point + vertex)->x = center->x + *radius * cos(angle);
+        (point + vertex)->y = center->y + *radius * sin(angle);
+    }
 }
 
 void draw_polygon(Point *point) {
-  for (int vertex = 0; vertex < N_VERTICES; vertex++) {
-    gfx_line((point + vertex)->x, (point + vertex)->y, (point + (vertex+1)%N_VERTICES)->x, (point + (vertex+1)%N_VERTICES)->y, YELLOW);
-  }
+    for (int vertex = 0; vertex < N_VERTICES; vertex++) {
+        gfx_line((point + vertex)->x, (point + vertex)->y, (point + (vertex+1)%N_VERTICES)->x, (point + (vertex+1)%N_VERTICES)->y, YELLOW);
+    }
 }
 
